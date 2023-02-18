ecb83a872bbe71778593b768470e43992d7ce511
Ancient <immortalprophet11@gmail.com>
2023-02-18 14:09:07 +0000

    123

--- lesson.py
+++ lesson.py
@@ -1,1 +1,26 @@
-print('Hello World')
+ #Хотел сделать цикличным программу через - "while True:, break"
+ #Но сейчас нет времени чтобы разобраться в причине неверного выполнения команды.
+result = 0
+try:
+    value_1 = float(input("Введите первое число: "))
+    value_2 = float(input("Введите второе число: "))
+
+    operation = input("Выберите одну из опираций: ('+', '-', '*', '/')\n")
+    if operation == '+':
+        result = value_1 + value_2
+        print('Ответ: ')
+    elif operation == '-':
+        result = value_1 - value_2
+        print('Ответ: ')
+    elif operation == '*':
+        result = value_1 * value_2
+        print('Ответ: ')
+    elif operation == '/':
+        result = value_1 / value_2
+
+    print(result)
+
+except ValueError:
+    quit("Ошибка ввода данных")
+except ZeroDivisionError:
+    quit("Не дели меня на ноль")
