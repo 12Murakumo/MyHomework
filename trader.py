7312c17c4a3a463a8470180254a8ec7e0779b06a
Ancient <immortalprophet11@gmail.com>
2023-03-31 17:03:49 +0100

    123

--- trader.py
+++ trader.py
@@ -20,34 +20,55 @@ def get_rate(state):
 def get_available(state):
     print(f"USD {round(state['usd_balance'], 2)} UAH {round(state['uah_balance'], 2)}")
 
+def log_transaction(transaction_type, amount, state):
+    with open('history.txt', 'a') as f:
+        f.write(f"{transaction_type} {amount} USD_RATE {round(state['usd_rate'], 2)} "
+                f"USD_BALANCE {round(state['usd_balance'], 2)} UAH_BALANCE {round(state['uah_balance'], 2)}\n")
 
 def buy_usd(amount, state):
-    if amount == 'ALL':
-        amount = state['uah_balance'] / state['usd_rate']
+    if amount.upper() == 'ALL':
+        if state['uah_balance'] >= state['usd_rate']:
+            amount = state['uah_balance'] // state['usd_rate']
+        else:
+            print("UNAVAILABLE, NOT ENOUGH UAH BALANCE TO BUY USD")
+            return
+    else:
+        amount = float(amount)
+        required_uah = amount * state['usd_rate']
+        if state['uah_balance'] >= required_uah:
+            state['usd_balance'] += amount
+            state['uah_balance'] -= required_uah
+            update_state(state)
+            log_transaction("BUY", amount, state)
+        else:
+            print(f"UNAVAILABLE, REQUIRED BALANCE UAH {round(required_uah, 2)},"
+            f" AVAILABLE {round(state['uah_balance'], 2)}")
+            return
+    print(f"Successfully bought {amount} USD")
+    get_available(state)
+
+
+
+def sell_usd(amount, state):
+    if amount.upper() == 'ALL':
+        amount = state['usd_balance']
     else:
         amount = float(amount)
 
-    required_uah = amount * state['usd_rate']
-    if state['uah_balance'] >= required_uah:
-        state['usd_balance'] += amount
-        state['uah_balance'] -= required_uah
-        update_state(state)
-    else:
-        print(f"UNAVAILABLE, REQUIRED BALANCE UAH {round(required_uah, 2)}, AVAILABLE {round(state['uah_balance'], 2)}")
-
-def sell_usd(amount, state):
-    if amount == 'ALL':
-        amount = state['usd_balance']
-
-    amount = float(amount)
     required_usd = amount
 
     if state['usd_balance'] >= required_usd:
         state['uah_balance'] += amount * state['usd_rate']
         state['usd_balance'] -= required_usd
         update_state(state)
+        log_transaction("SELL", amount, state)
+        print(f"Successfully sold {amount} USD")
+        get_available(state)
     else:
-        print(f"UNAVAILABLE, REQUIRED BALANCE USD {round(required_usd, 2)}, AVAILABLE {round(state['usd_balance'], 2)}")
+        print(
+            f"UNAVAILABLE, REQUIRED BALANCE USD {round(required_usd, 2)},"
+            f" AVAILABLE {round(state['usd_balance'], 2)}")
+
 
 
 def next_rate(state, config):
@@ -57,12 +78,14 @@ def next_rate(state, config):
 
 def restart(config):
     initial_state = {
-        "usd_rate": config['initial_usd_rate'],
-        "uah_balance": config['initial_uah_balance'],
-        "usd_balance": config['initial_usd_balance']
-    }
-    update_state(initial_state)
+    "usd_rate": config['initial_usd_rate'],
+    "uah_balance": config['initial_uah_balance'],
+    "usd_balance": config['initial_usd_balance']
+}
 
+    update_state(initial_state)
+    with open('history.txt', 'w') as f:
+        f.write('RESTART\n')
 def main():
     if len(sys.argv) < 2:
         print("Usage: python trader.py <command>")
@@ -96,8 +119,6 @@ def main():
     else:
         print('Unknown command')
 
+
 if __name__ == "__main__":
     main()
-
-
-
